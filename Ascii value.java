e0a8a8152389e523fe49ac17302d5d4c602987eb
gowtham05-hash <viji29783@gmail.com>
2024-09-14 08:58:27 +0530

    Add files via upload

---
+++ Ascii value.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Input_output {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char a = sc.next().charAt(0);
+		int n= (int)a;
+		System.out.println(n);
+	}
+
+}
